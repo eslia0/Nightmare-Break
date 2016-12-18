eb09b08a21668434e4b467bf0a35106063d3097c
junhyuk <dds4120@naver.com>
2016-12-18 21:01:48 +0900

    1218 밤

---
+++ Assets/Scripts/Effect/SwordCircle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordCircle : MonoBehaviour 
+{
+	public Renderer circle;
+	public Renderer circleChild;
+	public Material a;
+	public float alpha;
+	public float alphaChild;
+	// Use this for initialization
+	void Start () 
+	{
+		circle = this.GetComponent<Renderer> ();
+		alpha = 1;
+		alphaChild = 0.3f;
+		StartCoroutine (EffectFadeOut());
+		//Destroy (this.gameObject, 4f);
+	}
+	IEnumerator	EffectFadeOut ()
+	{
+		yield return new WaitForSeconds (3f);
+		while (true)
+		{
+			alpha -= 0.1f;
+			alphaChild -= 0.1f;
+			circle.material.color = new Color (1f, 1f, 1f, alpha);
+			//circleChild.material.color = new Color (1f, 1f, 1f, (alpha-0.6f));
+			circleChild.material.SetColor ("_TintColor",new Color(1f,1f,1f, (alphaChild)));
+			yield return new WaitForSeconds (0.1f);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Effect/SwordEffectFadeOut.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordEffectFadeOut : MonoBehaviour
+{
+
+	public Renderer tempRender;
+	public Material a;
+	float alpha;
+	// Use this for initialization
+	void Start ()
+	{		
+		tempRender = GetComponent<Renderer> ();
+		alpha = 1;
+		StartCoroutine (EffectFadeOut());
+	}
+
+	IEnumerator	EffectFadeOut ()
+	{
+		while (true)
+		{
+			alpha -= 0.1f;
+	
+			tempRender.material.SetColor ("_TintColor",new Color(1f,1f,1f, alpha));
+			//a.SetColor ("_Tint Color", new Color(1,1,1,alpha));
+			// = new Color(1,1,1,alpha);
+
+
+			yield return new WaitForSeconds (0.04f);
+		}
+	}
+	
+	// Update is called once per frame
+
+}
