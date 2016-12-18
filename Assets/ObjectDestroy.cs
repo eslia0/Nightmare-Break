a53bc163a97ac4aa1a191e0cef8486b05dbf4e33
junhyuk <dds4120@naver.com>
2016-12-18 13:52:55 +0900

    1

---
+++ Assets/ObjectDestroy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy (this.gameObject);
+	}
+
+}
